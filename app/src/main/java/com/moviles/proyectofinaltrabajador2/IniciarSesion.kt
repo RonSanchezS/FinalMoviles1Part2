f669fbfefdad7f99ec834168ed8f24874d2c7cb1
RonSanchezS <58291957+RonSanchezS@users.noreply.github.com>
2022-12-14 16:18:52 -0400

    Commit inicial de Firebase

--- app/src/main/java/com/moviles/proyectofinaltrabajador2/IniciarSesion.kt
+++ app/src/main/java/com/moviles/proyectofinaltrabajador2/IniciarSesion.kt
@@ -4,10 +4,13 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.text.method.TextKeyListener.clear
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import com.moviles.proyectofinaltrabajador2.models.LoginResponse
 import com.moviles.proyectofinaltrabajador2.models.WorkerLogin
 import com.moviles.proyectofinaltrabajador2.repository.LoginRepository
@@ -18,6 +21,8 @@ class IniciarSesion : AppCompatActivity(), LoginRepository.onLoginListener {
     private lateinit var btnPass: EditText
     private lateinit var btnLogin: Button
     private lateinit var btnRegistrarseActivity: Button
+
+    private lateinit var tokenDeFirebase : String
 
     private lateinit var editor: SharedPreferences.Editor
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,14 +40,33 @@ class IniciarSesion : AppCompatActivity(), LoginRepository.onLoginListener {
         Toast.makeText(this, "Bienvenido $token", Toast.LENGTH_SHORT).show()
         setUpListView();
         setUpListeners()
+        setUpFirebase()
         //if there is a token in shared prefferences, go to homeActivity
+
+    }
+
+    private fun setUpFirebase() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("TOKEN", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            tokenDeFirebase = task.result as String
+
+            // Log and toast
+            Log.d("TOKEN", tokenDeFirebase)
+            Toast.makeText(baseContext, tokenDeFirebase, Toast.LENGTH_SHORT).show()
+            println("TOKEN DE FIREBASE ES: $tokenDeFirebase")
+        })
 
     }
 
     private fun setUpSharedPref() {
         val pref = applicationContext.getSharedPreferences("MyPref", 0) // 0 - for private mode
         editor = pref.edit()
-       
+
     }
 
     private fun setUpListeners() {
@@ -56,7 +80,7 @@ class IniciarSesion : AppCompatActivity(), LoginRepository.onLoginListener {
             if (mail.isEmpty() || pass.isEmpty()) {
                 return@setOnClickListener
             }
-            val worker = WorkerLogin(mail, pass, "test_notification_id")
+            val worker = WorkerLogin(mail, pass, tokenDeFirebase)
             //   Toast.makeText(this, "Login $worker", Toast.LENGTH_SHORT).show()
             LoginRepository.login(worker, this, this);
         }
